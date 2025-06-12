63ba800e8108b19917b2a3f7358f3e0f0616d9a3
vitor <ivitoror@gmail.com>
2025-06-12 09:31:20 -0300

    Alteracao na funcao sensorMovimento

--- src/sensorMovimento.cpp
+++ src/sensorMovimento.cpp
@@ -3,6 +3,8 @@
 
 Adafruit_VL53L0X lox;
 
+bool alteracaoMovimento = 0;
+
 
 
 void sensorMovimentoInit() {
@@ -19,17 +21,9 @@ void sensorMovimento() {
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
 
-  int distancia = measure.RangeMilliMeter / 10;
-
- /* if (distancia > 30 && distancia < 50) {
-    Serial.println("Entre 30 e 50");
-  }
-  if (distancia > 10 && distancia < 30) {
-    Serial.println("Entre 10 e 30");
-  }
-
-  */
-  if (measure.RangeStatus != 4) {  // 4 = erro
+  int distanciaMovimento = measure.RangeMilliMeter / 10;
+ 
+  /*if (measure.RangeStatus != 4) {  // 4 = erro
     Serial.print("Distância: ");
     Serial.print(measure.RangeMilliMeter);
     Serial.println(" mm");
@@ -37,7 +31,17 @@ void sensorMovimento() {
     Serial.println("CM");
   } else {
     Serial.println("Fora de alcance");
+  }*/
+
+  if(distanciaMovimento >30 && distanciaMovimento < 40)
+  {
+    alteracaoMovimento = 0;
   }
-  delay(500);
+  else 
+  {
+    alteracaoMovimento = 1;
+  }
+
+  Serial.println(alteracaoMovimento);
 }
 
